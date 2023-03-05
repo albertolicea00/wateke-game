1edc601ce080298be5eb7bb2611284171ab620d7
Alberto <albertolicea00@icloud.com>
2023-03-04 22:28:05 -0500

    first commit

---
+++ src/init.js
@@ -0,0 +1,30 @@
+const config = {
+    width: 320,
+    height: 180,
+    parent: "container",
+    pixerArt: true,
+    type: Phaser.AUTO,
+    scene: {
+        preload : preload,
+        create : create,
+        update : update,
+    }
+
+}
+
+var game = new Phaser.Game(config);
+
+function preload()
+{
+
+}
+
+function create()
+{
+
+}
+
+function update(time, delta)
+{
+
+}
